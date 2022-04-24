84f7c453d8094f36a2d4a0386c0e562cde28d942
Rodolfo Rafael Dzul Cetina <rdzul@palaceresorts.com>
2022-04-24 15:43:57 -0500

    deploy

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,7 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const path = require("path");
 module.exports = {
-  publicPath: process.env.NODE_ENV === "production" ? "/pokedex/" : "/pokedex/"
+  publicPath: process.env.NODE_ENV === "production" ? "/pokedex/" : "/pokedex/",
+  outputDir: path.resolve(__dirname, "./docs"),
+  assetsDir: ""
 }
