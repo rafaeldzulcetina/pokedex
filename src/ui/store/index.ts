c495dc75d6dee8f3246fa4fa0d3d3253069f0d14
Rodolfo Rafael Dzul Cetina <rdzul@palaceresorts.com>
2022-04-24 18:00:53 -0500

    Update index.ts

--- src/ui/store/index.ts
+++ src/ui/store/index.ts
@@ -1,23 +1,19 @@
 import Vue from "vue";
 import Vuex from "vuex";
 import PokedexStore from "./modules/pokedex/pokedexStore";
-import { getters } from "./getter";
-import { actions } from "./actions";
-import { mutations } from "./mutations";
-import { RootState } from "@/ui/store/types/RootState";
 
 Vue.use(Vuex);
 
-const store = new Vuex.Store<RootState>({
+const store = new Vuex.Store({
   state: {
     isLoading: false,
   },
   modules: {
     PokedexStore,
   },
-  getters,
-  actions,
-  mutations,
+  getters: {},
+  actions: {},
+  mutations: {},
 });
 
 export default store;
